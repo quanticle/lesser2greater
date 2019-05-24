e4434586c7ee9c4ed8ec9ecec09b0a72af8e8ab7
Rohit Patnaik <quanticle@quanticle.net>
2019-05-23 20:51:24 -0500

    Added Effective Altruism Forum support.

--- chrome/background.js
+++ chrome/background.js
@@ -1,24 +1,30 @@
-let checkUrl = true;
 chrome.webNavigation.onBeforeNavigate.addListener(async function(e) {
     let originalUrl = e.url;
-    let replaceRe = /(.*)(less(?:er)?wrong)(.*)/;
-    let newUrl = originalUrl.replace(replaceRe, '$1greaterwrong$3');
-    if(checkUrl) {
+    let replaceRePairs = [
+        [/(.*)(less(?:er)?wrong)(.*)/, '$1greaterwrong$3'],
+        [/forum.effectivealtruism\.org(.*)/, 'ea.greaterwrong.com$1']
+    ];
+    let newUrl = originalUrl;
+    replaceRePairs.forEach((pair) => {
+        if (newUrl === originalUrl) {
+            let [matchExpression, replaceString] = pair;
+            newUrl = originalUrl.replace(matchExpression, replaceString);
+        }
+    });
+    if(newUrl !== originalUrl){
         try {
             let response = await fetch(newUrl, {method: "HEAD"});
-
             if(!response.ok){
                 console.log("GreaterWrong doesn't support " + newUrl + ". Received status: " + response.status);
             }
             else {
                 chrome.tabs.update(e.tabId, {url: newUrl});
             }
-            
         }
         catch(err) {
             console.log("GreaterWrong doesn't support " + newUrl + ". Received error: " + err.toString());
-
         }
     }
 }, {url: [{hostContains: '.lesserwrong.com'},
-          {hostContains: 'lesswrong.com'}]});
+          {hostContains: 'lesswrong.com'},
+          {hostContains: 'forum.effectivealtruism.org'}]});
