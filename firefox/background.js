e7a3100593b304e4d46c446e63fff32f0fb2531a
Rohit Patnaik <quanticle@quanticle.net>
2024-04-21 23:03:38 -0500

    Added support for Alignment Forum and Arbital.

--- firefox/background.js
+++ firefox/background.js
@@ -1,30 +1,38 @@
-browser.webNavigation.onBeforeNavigate.addListener(async function(e) {
-    let originalUrl = e.url;
-    let replaceRePairs = [
-        [/(.*)(less(?:er)?wrong)(.*)/, '$1greaterwrong$3'],
-        [/forum.effectivealtruism\.org(.*)/, 'ea.greaterwrong.com$1']
-    ];
-    let newUrl = originalUrl;
-    replaceRePairs.forEach((pair) => {
-        if (newUrl === originalUrl) {
-            let [matchExpression, replaceString] = pair;
-            newUrl = originalUrl.replace(matchExpression, replaceString);
-        }
-    });
-    if(newUrl !== originalUrl){
-        try {
-            let response = await fetch(newUrl, {method: "HEAD"});
-            if(!response.ok){
-                console.log("GreaterWrong doesn't support " + newUrl + ". Received status: " + response.status);
-            }
-            else {
-                browser.tabs.update(e.tabId, {url: newUrl});
-            }
-        }
-        catch(err) {
-            console.log("GreaterWrong doesn't support " + newUrl + ". Received error: " + err.toString());
-        }
+browser.webNavigation.onBeforeNavigate.addListener(async function (e) {
+  let originalUrl = e.url;
+  let replaceRePairs = [
+    [/(.*)(less(?:er)?wrong)(.*)/, '$1greaterwrong$3'],
+    [/forum.effectivealtruism\.org(.*)/, 'ea.greaterwrong.com$1'],
+    [/alignmentforum\.org\/posts\/(.*)/, 'greaterwrong.com/posts/$1'],
+    [/alignmentforum\.org\/s\/(.*)/, 'greaterwrong.com/s/$1'],
+    [/alignmentforum\.org\/?/, 'greaterwrong.com/index?view=alignment-forum'],
+    [/arbital\.com(.*)/, 'arbital.greaterwrong.com$1']
+  ];
+  let newUrl = originalUrl;
+  replaceRePairs.forEach((pair) => {
+    if (newUrl === originalUrl) {
+      let [matchExpression, replaceString] = pair;
+      newUrl = originalUrl.replace(matchExpression, replaceString);
     }
-}, {url: [{hostContains: '.lesserwrong.com'},
-          {hostContains: 'lesswrong.com'},
-          {hostContains: 'forum.effectivealtruism.org'}]});
+  });
+  if (newUrl !== originalUrl) {
+    try {
+      let response = await fetch(newUrl, { method: "HEAD" });
+      if (!response.ok) {
+        console.log("GreaterWrong doesn't support " + newUrl + ". Received status: " + response.status);
+      }
+      else {
+        browser.tabs.update(e.tabId, { url: newUrl });
+      }
+    }
+    catch (err) {
+      console.log("GreaterWrong doesn't support " + newUrl + ". Received error: " + err.toString());
+    }
+  }
+}, {
+  url: [{ hostContains: '.lesserwrong.com' },
+        { hostContains: 'lesswrong.com' },
+        { hostContains: 'forum.effectivealtruism.org' },
+        { hostContains: 'alignmentforum.org' },
+        { hostContains: 'arbital.com' }]
+});
